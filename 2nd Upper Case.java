accad551054f1c10de910a75857b680d7e0d63b0
Vidhyaa2002 <88870010+Vidhyaa2002@users.noreply.github.com>
2023-02-09 14:28:47 +0530

    Add files via upload

---
+++ 2nd Upper Case.java
@@ -0,0 +1,14 @@
+ 
+
+{
+
+
+String strArray[] = input1.split(" ");
+        String stt = "LESS";
+        if(strArray.length == 1)
+        {
+            return stt;
+        }
+		return strArray[1].toUpperCase();
+	}
+}
